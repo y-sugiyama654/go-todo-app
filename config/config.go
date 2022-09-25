7863f6166958d6cf12eb881b9f262efaecb27e19
Yuta <y.sugiyama54@gmail.com>
2022-09-26 06:58:36 +0900

    configの設定

---
+++ config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"log"
+
+	"gopkg.in/ini.v1"
+)
+
+type ConfigList struct {
+	Port      string
+	SQLDriver string
+	DbName    string
+	LogFile   string
+}
+
+// グローバルにConfigを定義
+var Config ConfigList
+
+// init処理でLoadConfig関数を実行
+func init() {
+	LoadConfig()
+}
+
+func LoadConfig() {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// 読み込んだconfigをグローバルに設定
+	Config = ConfigList{
+		Port:      cfg.Section("web").Key("port").MustString("8080"),
+		SQLDriver: cfg.Section("db").Key("driver").String(),
+		DbName:    cfg.Section("db").Key("name").String(),
+		LogFile:   cfg.Section("web").Key("logfile").String(),
+	}
+}

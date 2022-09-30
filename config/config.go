4f93451a35d3ba8d2e2209a5d815b9208b6822c4
Yuta <y.sugiyama54@gmail.com>
2022-09-30 22:36:52 +0900

    jqueryとbootstrapの設定

--- config/config.go
+++ config/config.go
@@ -12,6 +12,7 @@ type ConfigList struct {
 	SQLDriver string
 	DbName    string
 	LogFile   string
+	Static    string
 }
 
 // グローバルにConfigを定義
@@ -35,5 +36,6 @@ func LoadConfig() {
 		SQLDriver: cfg.Section("db").Key("driver").String(),
 		DbName:    cfg.Section("db").Key("name").String(),
 		LogFile:   cfg.Section("web").Key("logfile").String(),
+		Static:    cfg.Section("web").Key("static").String(),
 	}
 }
--- main.go
+++ main.go
@@ -2,62 +2,11 @@ package main
 
 import (
 	"fmt"
+	"main/app/controllers"
 	"main/app/models"
 )
 
 func main() {
-	// fmt.Println(models.Db)
-	/*
-		fmt.Println(config.Config.Port)
-		fmt.Println(config.Config.SQLDriver)
-		fmt.Println(config.Config.DbName)
-		fmt.Println(config.Config.LogFile)
-
-		// ログ出力(動作確認用)
-		log.Println("test")
-
-		fmt.Println(models.Db)
-	*/
-
-	// u := &models.User{}
-	// u.Name = "Yuta"
-	// u.Email = "yuta@gmail.com"
-	// u.PassWord = "Test2test"
-	// // userの作成
-	// u.CreateUser()
-
-	// user, _ := models.GetUser(2)
-	// // totoの作成
-	// user.CreateTodo("First Todo")
-	// todo, _ := models.GetTodo(1)
-	// fmt.Println(todo)
-
-	// fmt.Println(u)
-
-	// u.Name = "Test2"
-	// u.Email = "test2@example.com"
-	// u.UpdateUser()
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-	// u.DeleteUser()
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-	// todo, _ := models.GetTodo(1)
-	// fmt.Println(todo)
-
-	// user, _ := models.GetUser(2)
-	// todos, _ := user.GetTodosByUser()
-	// for _, v := range todos {
-	// 	fmt.Println(v)
-	// }
-
-	t, _ := models.GetTodo(2)
-	t.Content = "Second Todo"
-	t.UpdateTodo()
-	t, _ = models.GetTodo(2)
-	fmt.Println(t)
-	// fmt.Println(t)
-	// t.DeleteTodo()
+	fmt.Println(models.Db)
+	controllers.StartMainServer()
 }
